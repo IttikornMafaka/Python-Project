e3f43ff58f352a956c3a176b4ea99798a2ddfe11
Chokunnotlikeus <ittikorn.chokun@gmail.com>
2025-11-06 22:27:48 +0800

    Add files via upload
    This Python program is a simple text-based adventure game.
    The player makes a series of choices that determine whether they find the treasure or lose the game.
    Each step depends on the previous one — if you make the wrong choice, the game ends immediately (“Game Over!”).

---
+++ Treasuregame.py
@@ -0,0 +1,56 @@
+print('''   ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----\
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------\
+ \_/__________________________________________________________________/    ''')
+
+print('Welcome to the Treasure Island.')
+print('Your mission is to find the treasure.')
+choice1 = input('Where you want to go ? Left / Right: ')
+
+if choice1 == 'Left':
+    print('You see a big house')
+    choice2 = input('You find a house. Do you want to go inside or stay outside? Inside / Outside: ')
+    
+    if choice2 == 'Inside':
+        print('You arrive at the treasure house.')
+        choice3 = input('You find 3 doors. Which door do you want to choose? Red / Blue / Yellow: ')
+        
+        if choice3 == 'Yellow':
+            print('You found the treasure room!')
+            choice4 = input('You find a treasure chest. Do you want to open it? Yes / No: ')
+            if choice4 == 'Yes':
+                print('You found the monster inside the chest!')
+                choice5 = input('You find a monster. Do you want to fight')
+                if choice5 == 'Fight':
+                        print('You Win! You found the treasure!')
+                else:
+                    print('Game Over! The monster defeated you.')
+            else:
+                print('Game Over! You missed the treasure.')
+        else:
+            print('Game Over! You choose the wrong door.')
+    else:
+        print('Game Over! You missed the treasure house.')
+else:
+    print('Game Over! You fell into a hole.')
