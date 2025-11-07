4400473a0e9e2162416d1243cab0ab736a81cbc1
Chokunnotlikeus <ittikorn.chokun@gmail.com>
2025-11-07 23:22:41 +0800

    Add files via upload
    🔐 Password Generator
    🧩 Description
    
    This project is a simple and interactive Password Generator built with Python.
    It allows users to create secure and randomized passwords by choosing the number of letters, symbols, and numbers they want.
    
    The program uses Python’s built-in random module to ensure each password is unique and unpredictable, making it perfect for learning about lists, loops, and randomization.
    
    ⚙️ Features
    
    ✅ Choose how many letters, numbers, and symbols you want in your password
    ✅ Automatically shuffles the order for maximum randomness
    ✅ Easy to use in any Python environment
    ✅ Simple, beginner-friendly code
    ✅ Outputs a secure password instantly

---
+++ GeneratePassword.py
@@ -0,0 +1,23 @@
+import random
+letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbol = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password_list = []
+for char in range(1,nr_letters + 1):
+    password_list.append(random.choice(letter))
+for char in range(1,nr_symbols + 1):
+    password_list.append(random.choice(symbol))
+for char in range(1,nr_numbers + 1):
+    password_list.append(random.choice(number))
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list :
+    password += char
+print(password)
