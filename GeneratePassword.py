9138b003aba19ae5caddc9c4d78de7689eed170f
Chokunnotlikeus <ittikorn.chokun@gmail.com>
2025-11-07 23:23:50 +0800

    Update GeneratePassword.py

--- GeneratePassword.py
+++ GeneratePassword.py
@@ -7,7 +7,7 @@ print("Welcome to the Password Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-password_list = []
+password_list = [] #List Data from input
 for char in range(1,nr_letters + 1):
     password_list.append(random.choice(letter))
 for char in range(1,nr_symbols + 1):
@@ -15,8 +15,8 @@ for char in range(1,nr_symbols + 1):
 for char in range(1,nr_numbers + 1):
     password_list.append(random.choice(number))
 random.shuffle(password_list)
-print(password_list)
 
+# Convert List to char
 password = ""
 for char in password_list :
     password += char
